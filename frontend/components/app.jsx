c35e581ae8fe1df483fcad31717c9a5588f51635
Shahriver Hossain <s-hossain2015@nlaw.northwestern.edu>
2016-10-06 07:11:03 -0400

    add logo

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,9 +3,12 @@ import HeaderContainer from './header/header_container';
 
 const App = ({children}) => (
   <div>
-    <header className='group'>
+    <header>
+      <div className='header-nav group'>
+      <img className='header-logo' src={window.mediaAssets.logo} />
       <h1 className='header-title'>SIMPLENOTE</h1>
       <HeaderContainer />
+      </div>
     </header>
     {children}
   </div>
