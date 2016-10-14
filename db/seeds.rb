1ccd33ca98f85834e99ef4bdec1fd6e79a7f872c
Shahriver Hossain <s-hossain2015@nlaw.northwestern.edu>
2016-10-14 14:45:20 -0400

    fix bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,19 @@ Note.destroy_all
 Tag.destroy_all
 Tagging.destroy_all
 
-User.create(username: "guest", email: "guest-user@test.com", password: "password")
-User.create(username: "user1", email: "user1@test.com", password: "password")
-User.create(username: "app_student", email: "app_student@test.com", password: "password")
+author1 = User.create!(username: "guest", email: "guest-user@test.com", password: "password")
+author2 = User.create!(username: "user1", email: "user1@test.com", password: "password")
+author3 = User.create!(username: "app_student", email: "app_student@test.com", password: "password")
 
-Notebook.create(title: "Class", description: "important stuff from class", author_id: 1)
-Notebook.create(title: "Homework", description: "daily reports", author_id: 1)
-Notebook.create(title: "Exam Prep", description: "things to study", author_id: 1)
-Notebook.create(title: "Class", description: "important stuff from class", author_id: 3)
-Notebook.create(title: "Homework", description: "daily reports", author_id: 3)
-Notebook.create(title: "Exam Prep", description: "things to study", author_id: 3)
+notebook1 = author1.notebooks.create!(title: "Class", description: "important stuff from class")
+notebook2 = author1.notebooks.create!(title: "Homework", description: "daily reports")
+notebook3 = author1.notebooks.create!(title: "Exam Prep", description: "things to study")
+author3.notebooks.create!(title: "Class", description: "important stuff from class")
+author1.notebooks.create!(title: "Homework", description: "daily reports")
+author1.notebooks.create!(title: "Exam Prep", description: "things to study")
 
-Note.create(title: "References", body: "In the Ruby language variables hold references to objects.  One point is that = does not mutate (modify) an object; it merely reassigns the variable so that it now refers to a new object.", author_id: 1, notebook_id: 1)
-Note.create(title: "Array Methods", body: "Ruby array methods include: ", author_id: 1, notebook_id: 1)
-Note.create(title: "Mutable vs Immutable", body: "The Fixnum and Float classes are called immutable. None of their methods modify data inside the Fixnum/Float; they produce new values instead. nil is another example.", author_id: 1, notebook_id: 1)
-Note.create(title: "Review sorting algorithms", body: "Merge sort, Bubble sort, Quicksort", author_id: 1, notebook_id: 3)
-Note.create(title: "Javascript hw assignment", body: "install latest version of node!", author_id: 1, notebook_id: 2)
+notebook1.notes.create!(title: "References", body: "<div>In the Ruby language variables hold references to objects.  One point is that = does not mutate (modify) an object; it merely reassigns the variable so that it now refers to a new object.</div>", author_id: author1.id)
+notebook1.notes.create!(title: "Array Methods", body: "<div>Ruby array methods include: </div>", author_id: author1.id)
+notebook1.notes.create!(title: "Mutable vs Immutable", body: "<div>The Fixnum and Float classes are called immutable. None of their methods modify data inside the Fixnum/Float; they produce new values instead. nil is another example.</div>", author_id: author1.id)
+notebook3.notes.create!(title: "Review sorting algorithms", body: "<div>Merge sort, Bubble sort, Quicksort</div>", author_id: author1.id)
+notebook2.notes.create!(title: "Javascript hw assignment", body: "<div>install latest version of node!</div>", author_id: author1.id)
